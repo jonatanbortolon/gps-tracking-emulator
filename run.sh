844705013c6ac2a314be212d1f551520a2614d32
Jonatan Bortolon <jonatangabrielbortolon@outlook.com>
2024-09-24 00:58:25 -0300

    Change run bash script

--- run.sh
+++ run.sh
@@ -2,11 +2,29 @@
 
 if command -v meteor 2>&1 >/dev/null
 then
-    echo Meteor already installed
+    echo "=> Meteor already installed"
 else
-    echo Installing Meteor
+    echo "=> Installing Meteor"
 
     npx meteor
 fi
 
-NODE_TLS_REJECT_UNAUTHORIZED=0 meteor --port 3001
+url="http://localhost:9876/"
+
+NODE_TLS_REJECT_UNAUTHORIZED=0 meteor --port 9876 | while IFS= read -r i
+do 
+    if ! [[ $(echo "${i}" | grep -i "\[\[\[\[\[") ]]
+    then
+        echo "${i/Started/Starting}"
+    fi
+
+    if [[ $(echo "${i}" | grep -i "=> App running at: ${url}") ]]
+    then
+        if [[ $(grep -i Microsoft /proc/version) ]]
+        then
+            cmd.exe /C start "${url}" &> /dev/null
+        else
+            xdg-open "${url}" &> /dev/null
+        fi
+    fi
+done
